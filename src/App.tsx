2c39b3ea574e3b7f533b14208fb09ed0c8198cc5
Brian Prost <me@brianprost.com>
2024-05-22 00:57:33 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { UploadForm } from "./components/upload-form";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <UploadForm />
+      <Analytics />
     </>
   );
 }
