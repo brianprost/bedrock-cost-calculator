fa78ea2e44f1f08ce54f91525ce9c26665bacb91
Brian Prost <me@brianprost.com>
2024-06-12 20:05:21 -0400

    anthropic models

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -3,7 +3,10 @@ export type ModelName =
   | "mistral.mixtral-8x7b-instruct-v0:1"
   | "mistral.mistral-large-2402-v1:0"
   | "meta.llama3-8b-instruct-v1:0"
-  | "meta.llama3-70b-instruct-v1:0";
+  | "meta.llama3-70b-instruct-v1:0"
+  | "anthropic.claude-3-haiku-20240307-v1:0"
+  | "anthropic.claude-3-sonnet-20240229-v1:0"
+  | "anthropic.claude-3-opus-20240229-v1:0";
 
 export const models: ModelName[] = [
   "mistral.mistral-7b-instruct-v0:2",
@@ -11,6 +14,9 @@ export const models: ModelName[] = [
   "mistral.mistral-large-2402-v1:0",
   "meta.llama3-8b-instruct-v1:0",
   "meta.llama3-70b-instruct-v1:0",
+  "anthropic.claude-3-haiku-20240307-v1:0",
+  "anthropic.claude-3-sonnet-20240229-v1:0",
+  "anthropic.claude-3-opus-20240229-v1:0",
 ];
 
 const prices: Record<ModelName, { input: number; output: number }> = {
@@ -34,6 +40,18 @@ const prices: Record<ModelName, { input: number; output: number }> = {
     input: 0.00265,
     output: 0.0035,
   },
+  "anthropic.claude-3-haiku-20240307-v1:0": {
+    input: 0.00025,
+    output: 0.00125,
+  },
+  "anthropic.claude-3-sonnet-20240229-v1:0": {
+    input: 0.003,
+    output: 0.015,
+  },
+  "anthropic.claude-3-opus-20240229-v1:0": {
+    input: 0.015,
+    output: 0.075,
+  },
 };
 
 type ModelLabel =
@@ -41,7 +59,10 @@ type ModelLabel =
   | "Mixtral 8x7B"
   | "Mistral Large"
   | "Llama3 8B"
-  | "Llama3 70B";
+  | "Llama3 70B"
+  | "Claude 3 Haiku"
+  | "Claude 3 Sonnet"
+  | "Claude 3 Opus";
 
 export const getModelLabel = (model: ModelName): ModelLabel => {
   switch (model) {
@@ -55,6 +76,12 @@ export const getModelLabel = (model: ModelName): ModelLabel => {
       return "Llama3 8B";
     case "meta.llama3-70b-instruct-v1:0":
       return "Llama3 70B";
+    case "anthropic.claude-3-haiku-20240307-v1:0":
+      return "Claude 3 Haiku";
+    case "anthropic.claude-3-sonnet-20240229-v1:0":
+      return "Claude 3 Sonnet";
+    case "anthropic.claude-3-opus-20240229-v1:0":
+      return "Claude 3 Opus";
   }
 };
 
@@ -70,3 +97,29 @@ export const getModelPrice = (
 export const countTokens = (text: string | null, encoding: any): number => {
   return text ? encoding.encode(text).length : 0;
 };
+
+// (async () => {
+//   const inputTokenCount = 585 * 6300000;
+//   const outputTokenCount = 10 * 6300000;
+
+//   const inputPriceCalculations: { [key: string]: string } = {};
+//   models.forEach((model) => {
+//     inputPriceCalculations[model] = `$${getModelPrice(
+//       model,
+//       inputTokenCount,
+//       "input"
+//     ).toFixed(2)}`;
+//   });
+
+//   const outputPriceCalculations: { [key: string]: string } = {};
+//   models.forEach((model) => {
+//     outputPriceCalculations[model] = `$${getModelPrice(
+//       model,
+//       outputTokenCount,
+//       "output"
+//     ).toFixed(2)}`;
+//   });
+
+//   console.log({ inputPriceCalculations });
+//   console.log({ outputPriceCalculations });
+// })();
